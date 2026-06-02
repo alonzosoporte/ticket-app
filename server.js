0d72f007c35762a434f9318d3a7de9daac580084
Julian <alonzosoporte@gmail.com>
2026-06-02 11:34:09 -0300

    fix pdf

--- server.js
+++ server.js
@@ -580,14 +580,12 @@ app.get('/garantias', auth, async (req, res) => {
 
   try{
 
-    const data =
-    await Cotizacion.find({
+const data =
+await Cotizacion.find({
 
-      confirmada:'si'
-        fotoGarantia:'',
-        fotosGarantia:[]
+  confirmada:'si'
 
-    }).sort({ _id:-1 })
+}).sort({ _id:-1 })
 
     res.json(data)
 
